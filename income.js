f5fc93fa95e024d141f104b97e0307b6ee19e373
Andres Kim <andreskimlee@gmail.com>
2019-10-15 14:49:52 -0400

    Add dataset for income and circle visualization

---
+++ income.js
@@ -0,0 +1,310 @@
+export default {
+  "1": {
+    "Country": "Monaco",
+    "Average income": "186,080 $"
+  },
+  "2": {
+    "Country": "Liechtenstein",
+    "Average income": "116,430 $"
+  },
+  "3": {
+    "Country": "Bermuda",
+    "Average income": "106,140 $"
+  },
+  "4": {
+    "Country": "Switzerland",
+    "Average income": "83,580 $"
+  },
+  "5": {
+    "Country": "Norway",
+    "Average income": "80,790 $"
+  },
+  "6": {
+    "Country": "Macao",
+    "Average income": "78,320 $"
+  },
+  "7": {
+    "Country": "Luxembourg",
+    "Average income": "69,420 $"
+  },
+  "8": {
+    "Country": "Iceland",
+    "Average income": "67,950 $"
+  },
+  "9": {
+    "Country": "United States",
+    "Average income": "62,850 $"
+  },
+  "10": {
+    "Country": "Denmark",
+    "Average income": "60,190 $"
+  },
+  "11": {
+    "Country": "Ireland",
+    "Average income": "59,770 $"
+  },
+  "12": {
+    "Country": "Singapore",
+    "Average income": "58,770 $"
+  },
+  "13": {
+    "Country": "Sweden",
+    "Average income": "55,040 $"
+  },
+  "14": {
+    "Country": "Australia",
+    "Average income": "53,190 $"
+  },
+  "15": {
+    "Country": "Netherlands",
+    "Average income": "51,260 $"
+  },
+  "16": {
+    "Country": "Hong Kong",
+    "Average income": "50,310 $"
+  },
+  "17": {
+    "Country": "Austria",
+    "Average income": "49,260 $"
+  },
+  "18": {
+    "Country": "Finland",
+    "Average income": "47,750 $"
+  },
+  "19": {
+    "Country": "Germany",
+    "Average income": "47,180 $"
+  },
+  "20": {
+    "Country": "Belgium",
+    "Average income": "45,340 $"
+  },
+  "21": {
+    "Country": "Canada",
+    "Average income": "44,860 $"
+  },
+  "22": {
+    "Country": "Japan",
+    "Average income": "41,340 $"
+  },
+  "23": {
+    "Country": "United Kingdom",
+    "Average income": "41,340 $"
+  },
+  "24": {
+    "Country": "France",
+    "Average income": "41,080 $"
+  },
+  "25": {
+    "Country": "United Arab Emirates",
+    "Average income": "41,010 $"
+  },
+  "26": {
+    "Country": "Israel",
+    "Average income": "40,850 $"
+  },
+  "27": {
+    "Country": "New Zealand",
+    "Average income": "40,820 $"
+  },
+  "28": {
+    "Country": "Italy",
+    "Average income": "33,540 $"
+  },
+  "29": {
+    "Country": "South Korea",
+    "Average income": "30,600 $"
+  },
+  "30": {
+    "Country": "Spain",
+    "Average income": "29,450 $"
+  },
+  "31": {
+    "Country": "Portugal",
+    "Average income": "21,680 $"
+  },
+  "32": {
+    "Country": "Saudi Arabia",
+    "Average income": "21,540 $"
+  },
+  "33": {
+    "Country": "Czechia",
+    "Average income": "20,260 $"
+  },
+  "34": {
+    "Country": "Greece",
+    "Average income": "19,600 $"
+  },
+  "35": {
+    "Country": "Latvia",
+    "Average income": "16,740 $"
+  },
+  "36": {
+    "Country": "Chile",
+    "Average income": "14,670 $"
+  },
+  "37": {
+    "Country": "Hungary",
+    "Average income": "14,590 $"
+  },
+  "38": {
+    "Country": "Poland",
+    "Average income": "14,150 $"
+  },
+  "39": {
+    "Country": "Croatia",
+    "Average income": "13,830 $"
+  },
+  "40": {
+    "Country": "Venezuela",
+    "Average income": "13,080 $"
+  },
+  "41": {
+    "Country": "Argentina",
+    "Average income": "12,370 $"
+  },
+  "42": {
+    "Country": "Costa Rica",
+    "Average income": "11,510 $"
+  },
+  "43": {
+    "Country": "Romania",
+    "Average income": "11,290 $"
+  },
+  "44": {
+    "Country": "Malaysia",
+    "Average income": "10,460 $"
+  },
+  "45": {
+    "Country": "Turkey",
+    "Average income": "10,380 $"
+  },
+  "46": {
+    "Country": "Russia",
+    "Average income": "10,230 $"
+  },
+  "47": {
+    "Country": "China",
+    "Average income": "9,470 $"
+  },
+  "48": {
+    "Country": "Mexico",
+    "Average income": "9,180 $"
+  },
+  "49": {
+    "Country": "Brazil",
+    "Average income": "9,140 $"
+  },
+  "50": {
+    "Country": "Bulgaria",
+    "Average income": "8,860 $"
+  },
+  "51": {
+    "Country": "Kazakhstan",
+    "Average income": "7,830 $"
+  },
+  "52": {
+    "Country": "Equatorial Guinea",
+    "Average income": "7,050 $"
+  },
+  "53": {
+    "Country": "Thailand",
+    "Average income": "6,610 $"
+  },
+  "54": {
+    "Country": "Peru",
+    "Average income": "6,530 $"
+  },
+  "55": {
+    "Country": "Serbia",
+    "Average income": "6,390 $"
+  },
+  "56": {
+    "Country": "Colombia",
+    "Average income": "6,190 $"
+  },
+  "57": {
+    "Country": "Ecuador",
+    "Average income": "6,120 $"
+  },
+  "58": {
+    "Country": "South Africa",
+    "Average income": "5,750 $"
+  },
+  "59": {
+    "Country": "Bosnia and Herzegovina",
+    "Average income": "5,690 $"
+  },
+  "60": {
+    "Country": "Iran",
+    "Average income": "5,470 $"
+  },
+  "61": {
+    "Country": "Albania",
+    "Average income": "4,860 $"
+  },
+  "62": {
+    "Country": "Indonesia",
+    "Average income": "3,840 $"
+  },
+  "63": {
+    "Country": "Philippines",
+    "Average income": "3,830 $"
+  },
+  "64": {
+    "Country": "Morocco",
+    "Average income": "3,090 $"
+  },
+  "65": {
+    "Country": "Egypt",
+    "Average income": "2,800 $"
+  },
+  "66": {
+    "Country": "Ukraine",
+    "Average income": "2,660 $"
+  },
+  "67": {
+    "Country": "Vietnam",
+    "Average income": "2,400 $"
+  },
+  "68": {
+    "Country": "India",
+    "Average income": "2,020 $"
+  },
+  "69": {
+    "Country": "Nigeria",
+    "Average income": "1,960 $"
+  },
+  "70": {
+    "Country": "Bangladesh",
+    "Average income": "1,750 $"
+  },
+  "71": {
+    "Country": "Kenya",
+    "Average income": "1,620 $"
+  },
+  "72": {
+    "Country": "Pakistan",
+    "Average income": "1,580 $"
+  },
+  "73": {
+    "Country": "Ethiopia",
+    "Average income": "790 $"
+  },
+  "74": {
+    "Country": "Afghanistan",
+    "Average income": "550 $"
+  },
+  "75": {
+    "Country": "Democratic Republic of the Congo",
+    "Average income": "490 $"
+  },
+  "76": {
+    "Country": "Mozambique",
+    "Average income": "440 $"
+  },
+  "77": {
+    "Country": "Madagascar",
+    "Average income": "440 $"
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,110 @@
 import './styles/index.scss';
+import income from "../income"
 
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('app').innerText = "Hello World!";
-});
+(function() {
+  var width = 1500;
+  var height = 500;
+
+  var svg = d3.select("body") 
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width)
+    .append("g")
+    .attr("transform", "translate(0,0)")
+    // debugger 
+    console.log(income) 
+  
+  var simulation = d3.forceSimulation()
+    .force("x", d3.forceX(width/2).strength(0.02))
+    .force("y", d3.forceY(height/2).strength(0.02))
+  d3.queue()
+    .defer(d3.csv, "income.csv")
+    .await(ready) 
+
+
+  function ready (error, datapoints) {
+      var circles = svg.selectAll(".country")
+        .data(datapoints)
+        .enter().append("circle")
+        .attr("class", "country")
+        .attr("r", 10) 
+        .attr("fill", "lightblue")
+        
+        simulation.nodes(datapoints) //nodes refers to each circle 
+          .on('tick', ticked)
+        function ticked() { //magic boilerplate.... 
+          circles
+            .attr("cx", function(d) {
+              return d.x
+            })
+            .attr("cy", function(d) {
+              return d.y 
+            } )
+        }
+  }
+})();
+
+
+
+
+// function createBubbleChart(errors, income) {
+//   return d3.select("body").append("svg")
+// }
+
+// const generateRandomPositions = () => {
+//   const positions = [] 
+
+//   for (let i = 0; i< 20; i++) {
+//     positions[i] = {
+//       cx: Math.floor(Math.random() * 400),
+//       cy: Math.floor(Math.random() * 300),
+//       r: Math.floor(Math.random() * 100)
+//     }
+//   }
+//   return positions 
+// }
+
+// var svg = d3.select("body")
+//   .append("svg")
+
+
+// const circle = svg
+//   .selectAll("circle")
+//   .data(generateRandomPositions())
+//   .enter()
+//   .append("circle")
+//   .attr('cx', d => d.cx)
+//   .attr('cy', d => d.cy)
+//   .attr("r", d => d.r)
+//   .attr("fill", "url(#saitama)")
+
+
+// var defs = svg.append("defs") 
+// defs.append("pattern")
+//     .attr("id", "saitama")
+//     .attr("height", "100%")
+//     .attr("width", "100%")
+//     .attr("patternContentUnits", "objectBoundingBox")
+//     .append("image")
+//     .attr("height", 1)
+//     .attr("width", 1)
+//     .attr("preserveAspectRatio", "none")
+//     .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+//     .attr("xlink:href", "./src/images/saitama.jpg");
+
+// svg.attr("height", 1000)
+// svg.attr("width", 1000) 
+
+// var circles = svg.append("circle")
+//   .attr("class", "saitama")
+//   .attr("fill", "url(#saitama)")
+//   .attr("r", 50)
+//   .attr("cx", 50)
+//   .attr("cy", 100)
+
+// var circle2 = svg.append("circle")
+//   .attr("class", "abc")
+//   .attr("fill", "black")
+//   .attr("r", 50)
+//   .attr("cx", 400)
+//   .attr("cy", 400)
