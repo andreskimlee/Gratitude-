32b6fbc8e0994e2ddc0fe6f03b1bb9098514efa1
Andres Kim <andreskimlee@gmail.com>
2019-10-15 21:36:15 -0400

    Add input field with styling + eventhandler

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import income from "../income"
 
 (function() {
   var width = 1500;
-  var height = 650;
+  var height = 800;
 
   var svg = d3.select("body") 
     .append("svg")
@@ -31,8 +31,8 @@ import income from "../income"
 
 
   var simulation = d3.forceSimulation()
-    .force("x", d3.forceX(width/2).strength(0.1)) // move to svg width center x
-    .force("y", d3.forceY(height/2).strength(0.1)) // move to svg height center y
+    .force("x", d3.forceX(width/2).strength(0.05)) // move to svg width center x
+    .force("y", d3.forceY(height/2).strength(0.05)) // move to svg height center y
     .force("collide", d3.forceCollide(function(d) { 
       var formatted = ((d.Monthly.replace("$", ""))) 
       formatted = Number((formatted.replace(",", "")))
@@ -59,13 +59,19 @@ import income from "../income"
           formatted = Number((formatted.replace(",", "")))
               return radiusScale(formatted/10); 
         }) 
-        .attr("fill", "url(#saitama)")
+        .attr("fill", "black")
+        .style("opacity", .2) 
         .on("click", function(d){
           console.log(d)   // refactor to open a modal with the country of choice 
         })
-        .attr("stroke", "black")
+        // .attr("stroke", "black")
         
-        
+      // d3.select("#income-field").on()    
+    
+
+    // input field event handling.
+      
+
 
         simulation.nodes(datapoints) //nodes refers to each circle 
           .on('tick', ticked)
@@ -81,14 +87,20 @@ import income from "../income"
 
 
       
-
+        var input = document.getElementsByClassName("input-field");
+         
+        input[0].addEventListener("keyup", function(event) {
+          if (event.keyCode === 13) {
+            event.preventDefault();
+            console.log(input[0].value)
+            input[0].value = "" 
+          }
+        });
 
 
 
   }
 
-  
-  
 })();
 
 
@@ -103,64 +115,5 @@ import income from "../income"
 
 
 
-// function createBubbleChart(errors, income) {
-//   return d3.select("body").append("svg")
-// }
-
-// const generateRandomPositions = () => {
-//   const positions = [] 
-
-//   for (let i = 0; i< 20; i++) {
-//     positions[i] = {
-//       cx: Math.floor(Math.random() * 400),
-//       cy: Math.floor(Math.random() * 300),
-//       r: Math.floor(Math.random() * 100)
-//     }
-//   }
-//   return positions 
-// }
-
-// var svg = d3.select("body")
-//   .append("svg")
 
 
-// const circle = svg
-//   .selectAll("circle")
-//   .data(generateRandomPositions())
-//   .enter()
-//   .append("circle")
-//   .attr('cx', d => d.cx)
-//   .attr('cy', d => d.cy)
-//   .attr("r", d => d.r)
-//   .attr("fill", "url(#saitama)")
-
-
-// var defs = svg.append("defs") 
-// defs.append("pattern")
-//     .attr("id", "saitama")
-//     .attr("height", "100%")
-//     .attr("width", "100%")
-//     .attr("patternContentUnits", "objectBoundingBox")
-//     .append("image")
-//     .attr("height", 1)
-//     .attr("width", 1)
-//     .attr("preserveAspectRatio", "none")
-//     .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
-//     .attr("xlink:href", "./src/images/saitama.jpg");
-
-// svg.attr("height", 1000)
-// svg.attr("width", 1000) 
-
-// var circles = svg.append("circle")
-//   .attr("class", "saitama")
-//   .attr("fill", "url(#saitama)")
-//   .attr("r", 50)
-//   .attr("cx", 50)
-//   .attr("cy", 100)
-
-// var circle2 = svg.append("circle")
-//   .attr("class", "abc")
-//   .attr("fill", "black")
-//   .attr("r", 50)
-//   .attr("cx", 400)
-//   .attr("cy", 400)
