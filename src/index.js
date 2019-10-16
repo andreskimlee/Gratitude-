7ae80a9a6a044b081a9c9dfd436501e677055ef5
Andres Kim <andreskimlee@gmail.com>
2019-10-16 14:39:47 -0400

    Add Title

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import income from "../income"
 
 (function() {
   var width = 1500;
-  var height = 800;
+  var height = 765;
 
   var svg = d3.select("body") 
     .append("svg")
@@ -19,7 +19,7 @@ import income from "../income"
     .force("collide", d3.forceCollide(function(d) { 
       var formatted = ((d.Monthly.replace("$", ""))) 
       formatted = Number((formatted.replace(",", "")))
-      return radiusScale(formatted/10); 
+      return radiusScale(formatted/15); 
     }
     )) // makes sure circles dont overlap. Input the larget radius
   var radiusScale = d3.scaleSqrt().domain([3,1500]).range([10,90]) // domain refers to thousands (dollars) 
@@ -27,7 +27,7 @@ import income from "../income"
   d3.queue()
     .defer(d3.csv, "income.csv")
     .await(ready) 
-
+  
 
   function ready (error, datapoints) {
       var circles = svg.selectAll(".country")
@@ -38,7 +38,7 @@ import income from "../income"
           // console.log(d.Monthly.slice)
           var formatted = ((d.Monthly.replace("$", ""))) 
           formatted = Number((formatted.replace(",", "")))
-              return radiusScale(formatted/15); 
+              return radiusScale(formatted/20); 
         }) 
         .attr("fill", "black")
         .style("opacity", .3) 
@@ -74,27 +74,38 @@ import income from "../income"
         input[0].addEventListener("keyup", function(event) {
           if (event.keyCode === 13) {
             event.preventDefault();
-            
-            simulation.force("x", forceX).alphaTarget(0.5).restart() 
             input[0].value = ""
-            d3.select("body").append("text")
+            generateLessGreaterText() //not important
+            simulation.force("x", forceX).alphaTarget(0.1).restart() 
+            
+          }
+        });
+
+        function restart() {        
+          circles.enter().insert("circle")
+              .attr("class", "country")
+              .attr("r", 5)
+              .on("mousedown", mousedownNode);        
+      
+          simulation.start();
+        }
+        
+          
+
+
+
+        function generateLessGreaterText () {
+          d3.select("body").append("text")
               .text("Less")
               .attr("class", "lesser")
               d3.select("body").append("text")
               .text("Greater")
               .attr("class", "greater")
-          }
-          
-          
-        });
+        }
 
   }
 
 })();
-
-
-
-
 
 
 
